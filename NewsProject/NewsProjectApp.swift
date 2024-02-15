74a2e5726c00aa551296d01b31fe95529d12f770
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-15 09:16:22 +0000

    Initial Commit

---
+++ NewsProject/NewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsProjectApp.swift
+//  NewsProject
+//
+//  Created by Gabriela Ehrenbrink on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
