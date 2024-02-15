49b0f6c4bc4bbd5ac54c5d73e99231186714c018
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-15 11:53:05 +0000

    commit

--- NewsProject/Views/ContentView.swift
+++ NewsProject/Views/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider { // Fixed typo in struct name
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
